a14639fbe0b0f9d3fea2911ad2f584a5211aee04
Jossi Muñoz <jossimunoz@gmail.com>
2023-08-25 01:00:03 -0400

    final app

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -17,8 +17,16 @@ urlpatterns = [
     # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
-    # <HINT> Create a route for submit view
 
+
+    # <HINT> Create a route for submit view
+    path('<int:course_id>/submit_test/', views.submit, name='submit_test'),
     # <HINT> Create a route for show_exam_result view
+
+
+
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result')
+
+
 
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
